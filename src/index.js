5d34b04ffdbc6785c780cb524eace83d78d3c1ec
FilipToczynski <ftoczynski1998@gmail.com>
2022-08-01 11:41:28 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { LanguageContextProvider } from "./store/langContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <LanguageContextProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="https://toczynski.netlify.app/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="spotify" element={<Spotify />} />
