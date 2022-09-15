519cd3fbfe87c6e3c78b00e02649c94b62e84e8c
FilipToczynski <ftoczynski1998@gmail.com>
2022-09-15 21:04:35 +0200

    picture

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -14,17 +14,20 @@ const Hero = () => {
             Filip, I'm a web development enthusiast based in Wrocław, Poland.{" "}
             <br /> If you've time or interest to see my work, check below.
           </p>
-          <div className={styles.hero__image}>s</div>
+          <div className={styles.hero__image}></div>
         </div>
       )}
 
       {langctx.language && (
         <div className={styles.hero}>
           <p className={styles.hero__welcome}>
-              <span className={styles.hero__span}>Witam!</span> Mam na imię Filip,
-              jestem  Front end developerem z Wrocławia. <br /> Jak masz czas i chęci możesz zobaczyć moją pracę poniżej!
+            <span className={styles.hero__span}>Witam!</span> Mam na imię Filip,
+            jestem Front end developerem z Wrocławia. <br /> Jak masz czas i
+            chęci możesz zobaczyć moją pracę poniżej!
           </p>
-          <div className={styles.hero__image}>s</div>
+          <div className={styles.br}>
+            <div className={styles.hero__image}></div>
+          </div>
         </div>
       )}
     </>
